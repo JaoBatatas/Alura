47c981bf37a4c8950a28487d7c31dec2ecdd311c
João Paulo Ersching <joao.ersching@gmail.com>
2023-08-10 20:40:30 -0300

    const, final, var, dynamic list

--- Dart/first-project.dart
+++ Dart/first-project.dart
@@ -1,27 +1,33 @@
 import 'dart:math' as math;
 
 void main(){
-  String name = 'João Paulo Ersching';
-  String nick = 'JaoBatatas';
+  const String name = 'João Paulo Ersching';
+  final String nick;
   int age = 17;
   double height = 1.91;
   bool married = false;
 
   double bigNumber = 15e12;
+  const double pi = math.pi;
 
-  String sentence = 'Nome: $name \n'
-  'Nickname: $nick \n'
-  'Idade: $age \n'
-  'Altura: $height \n'
-  'Casado: $married \n'
+  nick = 'JaoBatatas';
+
+  // List<String> friends = ['Daniel', 'Henrique', 'Heitor', 'Guanho'];
+  // List friends = ['Daniel', 'Henrique', 'Heitor', 'Guanho', 124, 1.92];
+  List<dynamic> jota = [name, nick, age, height, married];
+
+  bigNumber = 19e10;
+
+  String sentence = 'Nome: ${jota[0]} \n'
+  'Nickname: ${jota[1]} \n'
+  'Idade: ${jota[2]} \n'
+  'Altura: ${jota[3]} \n'
+  'Casado: ${jota[4]} \n'
   'Número grande: $bigNumber \n'
-  'Pi: ${math.pi}';
-
-  List<String> friends = ['Daniel', 'Henrique', 'Heitor', 'Guanho'];
-  //List friends = ['Daniel', 'Henrique', 'Heitor', 'Guanho', 124, 1.92];
+  'Pi: ${pi}';
 
   print(sentence);
-  print(friends);
-  print(friends[0]);
-  print('Tamanho da lista: ${friends.length}');
+  // print(friends);
+  // print(friends[0]);
+  // print('Tamanho da lista: ${friends.length}');
 }

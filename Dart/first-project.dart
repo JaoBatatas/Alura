48cadfc86998b75de05b83f0505bf4e5cce1b423
João Paulo Ersching <joao.ersching@gmail.com>
2023-08-10 11:55:06 -0300

    int, string, double, bool, print, import

--- Dart/first-project.dart
+++ Dart/first-project.dart
@@ -1,3 +1,18 @@
+import 'dart:math' as math;
+
 void main(){
-  print('Hello World!');
+  String name = 'João Paulo Ersching';
+  int age = 17;
+  double height = 1.91;
+  bool married = false;
+
+  double bigNumber = 15e12;
+
+  print('Nome: $name');
+  print('Idade: $age');
+  print('Altura: $height');
+  print('Casado: $married');
+
+  print('Número grande: $bigNumber');
+  print('Pi: ${math.pi}');
 }

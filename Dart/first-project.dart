19e1a41585519c546f958933ea52e6625ef612e9
João Paulo Ersching <joao.ersching@gmail.com>
2023-08-14 21:55:22 -0300

    add if/else

--- Dart/first-project.dart
+++ Dart/first-project.dart
@@ -3,26 +3,36 @@ import 'dart:math' as math;
 void main(){
   const String name = 'João Paulo Ersching';
   final String nick;
+  bool adult;
   int age = 17;
   double height = 1.91;
   bool married = false;
+  bool geek = true;
 
   double bigNumber = 15e12;
   const double pi = math.pi;
 
   nick = 'JaoBatatas';
 
+  if(age >= 18){
+    adult = true;
+  } else{
+    adult = false;
+  }
+
   // List<String> friends = ['Daniel', 'Henrique', 'Heitor', 'Guanho'];
   // List friends = ['Daniel', 'Henrique', 'Heitor', 'Guanho', 124, 1.92];
-  List<dynamic> jota = [name, nick, age, height, married];
+  List<dynamic> jota = [name, nick, age, height, married, geek, adult];
 
   bigNumber = 19e10;
 
   String sentence = 'Nome: ${jota[0]} \n'
   'Nickname: ${jota[1]} \n'
+  'Maior de idade? ${jota[6]} \n'
   'Idade: ${jota[2]} \n'
   'Altura: ${jota[3]} \n'
-  'Casado: ${jota[4]} \n'
+  'Geek? ${jota[5]} \n'
+  'Casado? ${jota[4]} \n'
   'Número grande: $bigNumber \n'
   'Pi: ${pi}';
 

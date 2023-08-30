07f746445b0d59c3c7f256b19597ea29c2d8eabd
João Paulo Ersching <joao.ersching@gmail.com>
2023-08-29 21:26:40 -0300

    Add comments

--- Dart/first-project.dart
+++ Dart/first-project.dart
@@ -1,6 +1,7 @@
-import 'dart:math' as math;
+import 'dart:math' as math; //inclui biblioteca dart:math com o nome math
 
-void main(){
+void main() {
+  //informações sobre mim
   const String name = 'João Paulo Ersching';
   final String nick;
   bool adult;
@@ -14,47 +15,53 @@ void main(){
 
   nick = 'JaoBatatas';
 
-  if(age >= 18){
+  //verificação de maioridade
+  if (age >= 18) {
     adult = true;
-  } else{
+  } else {
     adult = false;
   }
 
-  for(int i = 1; i <= 10; i++){
+  //contador simples
+  for (int i = 1; i <= 10; i++) {
     print('Contador: $i');
   }
 
   int stamina = 100;
 
-  while(stamina > 0){
+  //while de treino e stamina
+  while (stamina > 0) {
     print('+Strength');
     stamina = stamina - math.Random().nextInt(6);
     print('Stamina: $stamina');
   }
 
-  int stamina2 = 100;
+  //do while de treino e stamina
+  // int stamina2 = 100;
 
-  do{
-    print('+Strength');
-    stamina2 = stamina2 - math.Random().nextInt(6);
-    print('Stamina2: $stamina2');
-  } while(stamina2 > 0);
+  // do {
+  //   print('+Strength');
+  //   stamina2 = stamina2 - math.Random().nextInt(6);
+  //   print('Stamina2: $stamina2');
+  // } while (stamina2 > 0);
 
+  //arrays de listas
   // List<String> friends = ['Daniel', 'Henrique', 'Heitor', 'Guanho'];
   // List friends = ['Daniel', 'Henrique', 'Heitor', 'Guanho', 124, 1.92];
   List<dynamic> jota = [name, nick, age, height, married, geek, adult];
 
-  bigNumber = 19e10;
+  bigNumber = 19e10; //outra maneira de escrever um numero grande
 
+  //frase a ser escrita
   String sentence = 'Nome: ${jota[0]} \n'
-  'Nickname: ${jota[1]} \n'
-  'Maior de idade? ${jota[6]} \n'
-  'Idade: ${jota[2]} \n'
-  'Altura: ${jota[3]} \n'
-  'Geek? ${jota[5]} \n'
-  'Casado? ${jota[4]} \n'
-  'Número grande: $bigNumber \n'
-  'Pi: ${pi}';
+      'Nickname: ${jota[1]} \n'
+      'Maior de idade? ${jota[6]} \n'
+      'Idade: ${jota[2]} \n'
+      'Altura: ${jota[3]} \n'
+      'Geek? ${jota[5]} \n'
+      'Casado? ${jota[4]} \n'
+      'Número grande: $bigNumber \n'
+      'Pi: ${pi}';
 
   print(sentence);
   // print(friends);

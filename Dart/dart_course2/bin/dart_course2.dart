26eef278189c219773e6dc760eeb7e028294fe66
João Paulo Ersching <joao.ersching@gmail.com>
2023-11-30 10:25:40 -0300

    Finish activity 5

--- Dart/dart_course2/bin/dart_course2.dart
+++ Dart/dart_course2/bin/dart_course2.dart
@@ -6,7 +6,61 @@ void main() {
   int daysSinceCrop = 10;
   bool isRipe = funcIsRipe(daysSinceCrop);
 
-  print(isRipe);
+  // print(isRipe);
+  // print(funcIsRipe(2));
+
+  // showRipe("morango", 80);
+  // showRipe("morango", 80, color: "vermelho");
+  // showRipe("morango", 80); || showRipe("morango", 80, color: "vermelho");
+  // showRipe("morango", 80, color: "vermelho"); // color é obrigatório
+}
+
+// Posicionais Obrigatórios
+// showRipe(String name, int days) {
+//   if (days >= 30) {
+//     print('A/O $name está madura/o!');
+//   } else {
+//     print('A/O $name não está madura/o!');
+//   }
+// }
+
+// Nomeados Opcionais
+// showRipe(String name, int days, {String? color}) {
+//   if (days >= 30) {
+//     print('A/O $name está madura/o!');
+//   } else {
+//     print('A/O $name não está madura/o!');
+//   }
+
+//   if (color != null) {
+//     print("A $name é $color.");
+//   }
+// }
+
+// Parâmetros com "Padrão"
+// showRipe(String name, int days, {String color = "azul"}) {
+//   if (days >= 30) {
+//     print('A/O $name está madura/o!');
+//   } else {
+//     print('A/O $name não está madura/o!');
+//   }
+
+//   if (color != null) {
+//     print("A $name é $color.");
+//   }
+// }
+
+// Modificador "Required"
+showRipe(String name, int days, {required String color}) {
+  if (days >= 30) {
+    print('A/O $name está madura/o!');
+  } else {
+    print('A/O $name não está madura/o!');
+  }
+
+  if (color != null) {
+    print("A $name é $color.");
+  }
 }
 
 bool funcIsRipe(int days) {

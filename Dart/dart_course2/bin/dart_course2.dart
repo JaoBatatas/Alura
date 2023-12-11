c859aa35f465bd08669f2937baed3c9a5ab54af3
João Paulo Ersching <joao.ersching@gmail.com>
2023-12-11 12:09:40 -0300

    another func

--- Dart/dart_course2/bin/dart_course2.dart
+++ Dart/dart_course2/bin/dart_course2.dart
@@ -3,7 +3,7 @@ void main() {
   double weight = 100.2;
   String color = "Azul e Roxa";
   String taste = "Doce e amarga";
-  int daysSinceCrop = 10;
+  int daysSinceCrop = 12;
   bool isRipe = funcIsRipe(daysSinceCrop);
 
   // print(isRipe);
@@ -13,45 +13,21 @@ void main() {
   // showRipe("morango", 80, color: "vermelho");
   // showRipe("morango", 80); || showRipe("morango", 80, color: "vermelho");
   // showRipe("morango", 80, color: "vermelho"); // color é obrigatório
+
+  print(funcHowMuchToRipe(daysSinceCrop));
 }
 
 // Posicionais Obrigatórios
-// showRipe(String name, int days) {
-//   if (days >= 30) {
-//     print('A/O $name está madura/o!');
-//   } else {
-//     print('A/O $name não está madura/o!');
-//   }
-// }
+/* showRipe(String name, int days) {
+  if (days >= 30) {
+    print('A/O $name está madura/o!');
+  } else {
+    print('A/O $name não está madura/o!');
+  }
+} */
 
 // Nomeados Opcionais
-// showRipe(String name, int days, {String? color}) {
-//   if (days >= 30) {
-//     print('A/O $name está madura/o!');
-//   } else {
-//     print('A/O $name não está madura/o!');
-//   }
-
-//   if (color != null) {
-//     print("A $name é $color.");
-//   }
-// }
-
-// Parâmetros com "Padrão"
-// showRipe(String name, int days, {String color = "azul"}) {
-//   if (days >= 30) {
-//     print('A/O $name está madura/o!');
-//   } else {
-//     print('A/O $name não está madura/o!');
-//   }
-
-//   if (color != null) {
-//     print("A $name é $color.");
-//   }
-// }
-
-// Modificador "Required"
-showRipe(String name, int days, {required String color}) {
+/* showRipe(String name, int days, {String? color}) {
   if (days >= 30) {
     print('A/O $name está madura/o!');
   } else {
@@ -61,7 +37,33 @@ showRipe(String name, int days, {required String color}) {
   if (color != null) {
     print("A $name é $color.");
   }
-}
+} */
+
+// Parâmetros com "Padrão"
+/* showRipe(String name, int days, {String color = "azul"}) {
+  if (days >= 30) {
+    print('A/O $name está madura/o!');
+  } else {
+    print('A/O $name não está madura/o!');
+  }
+
+  if (color != null) {
+    print("A $name é $color.");
+  }
+} */
+
+// Modificador "Required"
+/* showRipe(String name, int days, {required String color}) {
+  if (days >= 30) {
+    print('A/O $name está madura/o!');
+  } else {
+    print('A/O $name não está madura/o!');
+  }
+
+  if (color != null) {
+    print("A $name é $color.");
+  }
+} */
 
 bool funcIsRipe(int days) {
   if (days >= 7) {
@@ -69,4 +71,13 @@ bool funcIsRipe(int days) {
   } else {
     return false;
   }
+}
+
+funcHowMuchToRipe(int daysSincePlant) {
+  int daysToRipe = 30;
+  int howMuchToRipe = daysToRipe - daysSincePlant;
+
+  String sentenceToRipe = "Faltam $howMuchToRipe dias para ficar madura/o";
+  String sentenceAlreadyRipe = "Já está madura/o";
+  return howMuchToRipe > 0 ? sentenceToRipe : sentenceAlreadyRipe;
 }
